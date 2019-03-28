b8a26f48b7bd15e998093eb96cbe42393d4101b3
talmasc <eric.l.hillenbrand@gmail.com>
2019-03-28 15:38:40 -0600

    added routers and add/view author page

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,20 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read this page?')) {
+                transition.about();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('Are you sure you want to leave this page?')) {
+                transition.about();
+            } 
+        }
+    },
     render: function() {
         return (
             <div>
--- src/main.js
+++ src/main.js
@@ -1,36 +1,9 @@
-$ = jQuery = require('jquery');
-var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
-var Authors = require('./components/authors/authorPage');
+"use strict";
 
-(function(win) {
-    "use strict";
-    var App = React.createClass({
-        render: function() {
-            var Child;
-    
-            switch(this.props.route) {
-                case 'about': Child = About; break;
-                case 'authors': Child = Authors; break;
-                default: Child = Home;
-            }
-    
-            return (
-                <div>
-                    <Header/>
-                    <Child/>
-                </div>
-            );
-        }
-    });
-    
-    function render() {
-        var route = win.location.hash.substr(1);
-        React.render(<App route={route} />, document.getElementById('app'));
-    }
-    
-    window.addEventListener('hashchange', render);
-    render();
-})(window);
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
+
+Router.run(routes, function(Handler) {
+    React.render(<Handler />, document.getElementById('app'));
+});
